5f8f379f1b247d6a99c2f2e3429993389e321939
abdelrahman gamal <abdelrahmanbetter@stud.cu.edu.eg>
2023-08-15 00:55:09 +0300

    add basic variable and code in the game fragment .kt

--- app/src/main/java/com/example/guessinggame/game/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/game/GameFragment.kt
@@ -1,28 +1,40 @@
 package com.example.guessinggame.game
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.guessinggame.R
+import com.example.guessinggame.databinding.FragmentGameBinding
+import kotlin.random.Random
 
 class GameFragment : Fragment() {
-
+private var _binding:FragmentGameBinding?=null
+ private   val binding get() = _binding!!
+    private val TAG = "GameFragment"
+    val words= listOf<String>("Activity", "Kotlin","Fragment","Android")
+    val secret_word = words.random().uppercase()
+    var secret_word_Display=""
+    var correct_Guesses=""
+    var In_correct_Guesses=""
+    var livesleft=8
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding= FragmentGameBinding.inflate(inflater,container,false)
+        val view =binding.root
 
+
+        binding.guessesButton.setOnClickListener {
+            Log.i(TAG, "onCreateView: button press")
+        }
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val myviewmodel=ViewModelProvider(this).get(ViewModelGameFragment::class.java)
-
-    }
+  
 
 }
