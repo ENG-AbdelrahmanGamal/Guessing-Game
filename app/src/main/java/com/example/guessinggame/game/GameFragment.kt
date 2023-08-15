ad2da737a71b99b8426f9bc3eb9e7f95650f0726
abdelrahman gamal <abdelrahmanbetter@stud.cu.edu.eg>
2023-08-15 04:02:24 +0300

    the code without using view model

--- app/src/main/java/com/example/guessinggame/game/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/game/GameFragment.kt
@@ -7,6 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigatorDestinationBuilder
+import androidx.navigation.fragment.findNavController
 import com.example.guessinggame.R
 import com.example.guessinggame.databinding.FragmentGameBinding
 import kotlin.random.Random
@@ -19,7 +23,7 @@ private var _binding:FragmentGameBinding?=null
     val secret_word = words.random().uppercase()
     var secret_word_Display=""
     var correct_Guesses=""
-    var In_correct_Guesses=""
+    var in_correct_Guesses=""
     var livesleft=8
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +33,67 @@ private var _binding:FragmentGameBinding?=null
         val view =binding.root
 
 
+
+       secret_word_Display=driveSecritWordDisplayed()
+        updateSecret()
         binding.guessesButton.setOnClickListener {
-            Log.i(TAG, "onCreateView: button press")
+       makeGuess(binding.guesses.text.toString().uppercase())
+       binding.guesses.text=null
+       updateSecret()
+       if (isLost()||isWin())
+       {
+              //     val result= view.findNavController().navigate(isWinOrLost())
+           view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+
+     }
+
+
         }
+
         return view
     }
 
-  
+    private fun updateSecret() {
+     binding.word.text=secret_word_Display
+        binding.lives.text="you have $livesleft lives lift "
+    }
 
+    private fun driveSecritWordDisplayed(): String {
+        var display=""
+        secret_word.forEach { display+=checkLetter(it.toString()) }
+return display
+    }
+
+
+
+    private fun checkLetter(toString: String)=when(correct_Guesses.contains(toString)){
+
+true ->toString
+        false->"_"
+    }
+    fun makeGuess(guess:String) {
+        if (guess.length==1){
+            correct_Guesses+=guess
+            secret_word_Display=driveSecritWordDisplayed()
+
+        }else{
+            in_correct_Guesses+=guess
+            livesleft--
+        }
+    }
+    fun isWin() =secret_word.equals(secret_word_Display,true)
+    fun isLost()=livesleft<=0
+
+    fun isWinOrLost():String{
+        var message=""
+
+        if(isWin())message= "you are Win Congratulation"
+       else if(isLost())message="You are lost!"
+        message +="the correct word was $secret_word "
+        return message
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
+    }
 }
