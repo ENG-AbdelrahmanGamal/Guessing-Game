237bf9eefc8075cead53a010ae4f11fbdcbea525
abdelrahman gamal <abdelrahmanbetter@stud.cu.edu.eg>
2023-08-16 03:19:31 +0300

    create view model due to contral the business logic and data for the games fragment

--- app/src/main/java/com/example/guessinggame/game/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/game/GameFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorDestinationBuilder
@@ -19,28 +21,24 @@ class GameFragment : Fragment() {
 private var _binding:FragmentGameBinding?=null
  private   val binding get() = _binding!!
     private val TAG = "GameFragment"
-    val words= listOf<String>("Activity", "Kotlin","Fragment","Android")
-    val secret_word = words.random().uppercase()
-    var secret_word_Display=""
-    var correct_Guesses=""
-    var in_correct_Guesses=""
-    var livesleft=8
+ lateinit var viewmodel:ViewModelGameFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding= FragmentGameBinding.inflate(inflater,container,false)
         val view =binding.root
+        viewmodel=ViewModelProvider(this).get(ViewModelGameFragment::class.java)
 
 
 
-       secret_word_Display=driveSecritWordDisplayed()
+
         updateSecret()
         binding.guessesButton.setOnClickListener {
-       makeGuess(binding.guesses.text.toString().uppercase())
+       viewmodel.makeGuess(binding.guesses.text.toString().uppercase())
        binding.guesses.text=null
        updateSecret()
-       if (isLost()||isWin())
+       if (viewmodel.isLost()||viewmodel.isWin())
        {
               //     val result= view.findNavController().navigate(isWinOrLost())
            view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
@@ -54,44 +52,16 @@ private var _binding:FragmentGameBinding?=null
     }
 
     private fun updateSecret() {
-     binding.word.text=secret_word_Display
-        binding.lives.text="you have $livesleft lives lift "
-    }
-
-    private fun driveSecritWordDisplayed(): String {
-        var display=""
-        secret_word.forEach { display+=checkLetter(it.toString()) }
-return display
+        binding.word.text=viewmodel.secret_word_Display
+        binding.lives.text="you have ${viewmodel.livesleft}lives lift "
     }
 
 
 
-    private fun checkLetter(toString: String)=when(correct_Guesses.contains(toString)){
 
-true ->toString
-        false->"_"
-    }
-    fun makeGuess(guess:String) {
-        if (guess.length==1){
-            correct_Guesses+=guess
-            secret_word_Display=driveSecritWordDisplayed()
 
-        }else{
-            in_correct_Guesses+=guess
-            livesleft--
-        }
-    }
-    fun isWin() =secret_word.equals(secret_word_Display,true)
-    fun isLost()=livesleft<=0
 
-    fun isWinOrLost():String{
-        var message=""
 
-        if(isWin())message= "you are Win Congratulation"
-       else if(isLost())message="You are lost!"
-        message +="the correct word was $secret_word "
-        return message
-    }
     override fun onDestroy() {
         super.onDestroy()
         _binding=null
--- app/src/main/java/com/example/guessinggame/game/ViewModelGameFragment.kt
+++ app/src/main/java/com/example/guessinggame/game/ViewModelGameFragment.kt
@@ -6,8 +6,45 @@ import androidx.lifecycle.ViewModel
 
 class ViewModelGameFragment : ViewModel() {
     private  val TAG = "ViewModelGameFragment"
+    val words= listOf<String>("Activity", "Kotlin","Fragment","Android")
+    val secret_word = words.random().uppercase()
+    var secret_word_Display=""
+    var correct_Guesses=""
+    var in_correct_Guesses=""
+    var livesleft=8
 init {
-    Log.i(TAG, ":this is ViewModelGameFragment ")
-}
+    secret_word_Display=driveSecritWordDisplayed()
 
+}
+    private fun driveSecritWordDisplayed(): String {
+        var display=""
+        secret_word.forEach { display+=checkLetter(it.toString()) }
+        return display
+    }
+    private fun checkLetter(toString: String)=when(correct_Guesses.contains(toString)){
+
+        true ->toString
+        false->"_"
+    }
+    fun makeGuess(guess:String) {
+        if (guess.length==1){
+            correct_Guesses+=guess
+            secret_word_Display=driveSecritWordDisplayed()
+
+        }else{
+            in_correct_Guesses+=guess
+            livesleft--
+        }
+    }
+    fun isWin() =secret_word.equals(secret_word_Display,true)
+    fun isLost()=livesleft<=0
+
+    fun isWinOrLost():String{
+        var message=""
+
+        if(isWin())message= "you are Win Congratulation"
+        else if(isLost())message="You are lost!"
+        message +="the correct word was $secret_word "
+        return message
+    }
 }
